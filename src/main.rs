c0361150196e0c25283a47a84d9d41cc69d8c985
adamcanray <adamcool180@gmail.com>
2023-04-01 14:08:41 +0700

    simple demo to log an server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+#[macro_use] extern crate nickel;
 extern crate chrono;
 
 use std::io::prelude::*;
-use std::fs::{File,OpenOptions};
+use std::fs::{OpenOptions};
 use std::io;
 use chrono::{DateTime,Local};
 
+use nickel::Nickel;
 
 fn formatted_time_entry() -> String {
     let local: DateTime<Local> = Local::now();
@@ -22,17 +24,35 @@ fn record_entry_in_log(filename: &str, bytes: &[u8]) -> io::Result<()> {
     Ok(())
 }
 
-fn log_time(filename: &'static str) -> io::Result<()> {
+fn log_time(filename: &'static str) -> io::Result<String> {
     let entry = formatted_time_entry();
-    let bytes = entry.as_bytes();
+    // this actually works, but in some condition may it would throw this error: "error: cannot move out of `entry` because it is borrowed"
+    // let bytes = entry.as_bytes();
+    // r#try!(record_entry_in_log(filename, &bytes));
 
-    r#try!(record_entry_in_log(filename, &bytes));
-    Ok(())
+    // "stick a new block here" feature, so bellow is new block
+    {
+        let bytes = entry.as_bytes();
+        r#try!(record_entry_in_log(filename, &bytes));
+    }
+    Ok(entry)
+}
+
+fn do_log_time() -> String {
+    match log_time("log.txt") {
+        Ok(entry) => format!("Entry Logged: {}", entry),
+        Err(e) => format!("Error: {}", e)
+    }
 }
 
 fn main() {
-    match log_time("log.txt") {
-        Ok(..) => println!("File created!"),
-        Err(e) => println!("Error: {}", e)
-    }
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            do_log_time()
+        }
+    });
+
+    server.listen("127.0.0.1:6767").unwrap();
 }
